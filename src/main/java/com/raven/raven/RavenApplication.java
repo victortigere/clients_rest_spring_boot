fdcfcd0a623076653221f86a4271a5c1766e57ec
VTigere <tigerevictoremmanuel@outlook.com>
2021-02-15 07:46:21 +0200

    allows CORS on method level

--- src/main/java/com/raven/raven/RavenApplication.java
+++ src/main/java/com/raven/raven/RavenApplication.java
@@ -2,23 +2,16 @@ package com.raven.raven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("clients")
-public class RavenApplication {
 
+public class RavenApplication {
     public static void main(String[] args) {
         SpringApplication.run(RavenApplication.class, args);
     }
-
-//    Show all Clients
-    @GetMapping("/allclients")
-    public String getClients(){
-        return  "All Clients";
-    }
-
 }
--- src/main/java/com/raven/raven/controller/ClientController.java
+++ src/main/java/com/raven/raven/controller/ClientController.java
@@ -15,43 +15,49 @@ public class ClientController {
 
 //    save client
     @PostMapping("/saveclient")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Clients saveClient(@RequestBody Clients client){
         return clientService.saveClient(client);
     }
 
 //    save clients
     @PostMapping("/saveclients")
+    @CrossOrigin(origins = "http://localhost:4200")
     public List<Clients> saveClients(@RequestBody List<Clients> clients){
         return clientService.saveClients(clients);
     }
 
 //    get clients
     @GetMapping("/clients")
+    @CrossOrigin(origins = "http://localhost:4200")
     public List<Clients> getClients(){
         return clientService.getClients();
     }
 
 //    get client by id
     @GetMapping("/client/{id}")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Clients getClientById(@PathVariable  int id){
         return clientService.getClientsById(id);
     }
 
 //    get client by name
     @GetMapping("/clients/{name}")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Clients getClientByName(@PathVariable  String name){
         return clientService.getClientsByName(name);
     }
 
 //    delete client
     @DeleteMapping("/delete/client/{id}")
-    public String deleteClient(@PathVariable int id){
+    @CrossOrigin(origins = "http://localhost:4200")
+    public void deleteClient(@PathVariable int id){
         clientService.deleteClient(id);
-        return "Client deleted";
     }
 
 //    update Client
     @PutMapping("/updateclient")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Clients updateClient(@RequestBody Clients client){
         return clientService.updateClient(client);
     }
